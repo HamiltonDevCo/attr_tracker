045e77aac19d04854b2e8792f19c10714d6bed7e
Jonatan David Orozco Cantillo <jonatan.orozco@hamiltondevco.com>
2022-11-11 11:48:26 -0500

    fix: set migration version

--- db/migrate/20150830195702_add_attr_name_to_attr_tracker_changes.rb
+++ db/migrate/20150830195702_add_attr_name_to_attr_tracker_changes.rb
@@ -1,4 +1,4 @@
-class AddAttrNameToAttrTrackerChanges < ActiveRecord::Migration
+class AddAttrNameToAttrTrackerChanges < ActiveRecord::Migration[4.2]
   def change
     add_column :attr_tracker_changes, :attr_name, :string
   end
